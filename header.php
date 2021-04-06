753aff87bc999eccf305126b423a389c9a383fda
--get <sxhadixr@gmail.com>
2021-04-06 21:33:19 +0430

    Update header.php

--- header.php
+++ header.php
@@ -1,82 +1,154 @@
 <?
-include('header.php');
+if (!(isset($_SESSION['admin_id'])))
+    header("Location : index.php");
+
+include('class/database.php');
 ?>
-    <!-- Page wrapper  -->
-    <div class="page-wrapper">
 
-        <!-- Bread crumb -->
-        <div class="row page-titles">
-            <div class="col-md-5 align-self-center text-right">
-                <h3 class="text-primary">داشبورد</h3> </div>
-            <div class="col-md-7 align-self-center text-left">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="javascript:void(0)">خانه</a></li>
-                    <li class="breadcrumb-item active">داشبورد</li>
-                </ol>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>پنل مدیریت</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="//cdn.rawgit.com/morteza/bootstrap-rtl/v3.3.4/dist/css/bootstrap-rtl.min.css">
+    <!-- All Jquery -->
+    <script src="js/lib/jquery/jquery.min.js"></script>
+
+
+    <!-- select2 -->
+    <link href="css/select2.min.css" rel="stylesheet"/>
+    <script src="js/select2.min.js"></script>
+    <script src="js/select2.full.js"></script>
+
+    <link type="text/css" rel="stylesheet" href="css/kamadatepicker.css"/>
+
+    <script src="js/kamadatepicker.js"></script>
+
+
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <script>
+        // In your Javascript (external .js resource or <script> tag)
+        $(document).ready(function () {
+            $('.userlist').select2({
+                dir: "rtl"
+            });
+        });
+    </script>
+
+
+</head>
+
+
+<body class="fix-header fix-sidebar">
+<!-- Preloader - style you can find in spinners.css -->
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+    </svg>
+</div>
+<!-- Main wrapper  -->
+<div id="main-wrapper">
+    <!-- header header  -->
+    <div class="header">
+        <nav class="navbar top-navbar navbar-expand-md navbar-light">
+            <!-- Logo -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.html">
+                    <!-- Logo text -->
+                    <span><img src="images/logoofogh.png" alt="homepage" class="dark-logo"/></span>
+                </a>
             </div>
-        </div>
-        <!-- End Bread crumb -->
-        <!-- Container fluid  -->
-        <div class="container-fluid">
-            <!-- Start Page Content -->
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-usd f-s-40 color-primary"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>568120</h2>
-                                <p class="m-b-0">درآمد کل</p>
-                            </div>
+            <!-- End Logo -->
+            <div class="navbar-collapse">
+                <!-- toggle and nav items -->
+                <ul class="navbar-nav mr-auto mt-md-0">
+                    <!-- This is  -->
+                    <li class="nav-item"><a class="nav-link nav-toggler hidden-md-up text-muted  "
+                                            href="javascript:void(0)"><i class="mdi mdi-menu"></i></a></li>
+                    <li class="nav-item m-l-10"><a class="nav-link sidebartoggler hidden-sm-down text-muted  "
+                                                   href="javascript:void(0)"><i class="ti-menu"></i></a></li>
+
+                </ul>
+                <!-- User profile and search -->
+                <ul class="navbar-nav my-lg-0">
+
+
+                    <!-- Profile -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false"><img src="images/users/2.jpg" alt="user"
+                                                                           class="profile-pic"/></a>
+                        <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                            <ul class="dropdown-user">
+                                <li><a href="setting.php"><i class="ti-user"></i> پروفایل </a></li>
+                                <li><a href="setting.php"><i class="ti-settings"></i> تنظیمات</a></li>
+                                <li><a href="logout.php"><i class="fa fa-power-off"></i> خروج</a></li>
+                            </ul>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-shopping-cart f-s-40 color-success"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>1178</h2>
-                                <p class="m-b-0">فروش</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-archive f-s-40 color-warning"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>25</h2>
-                                <p class="m-b-0">سرویس کاران</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-user f-s-40 color-danger"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>847</h2>
-                                <p class="m-b-0">مشتریان</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </li>
+                </ul>
             </div>
+        </nav>
+    </div>
+    <!-- End header header -->
+    <!-- Left Sidebar  -->
+    <div class="left-sidebar">
+        <!-- Sidebar scroll-->
+        <div class="scroll-sidebar">
+            <!-- Sidebar navigation-->
+            <nav class="sidebar-nav">
+                <ul id="sidebarnav">
+                    <li class="nav-devider"></li>
+                    <li class="nav-label">خانه</li>
+                    <li><a class="has-arrow  " href="#" aria-expanded="false">
+                            <i class="fa fa-tachometer"></i>
+                            <span class="hide-menu">داشبورد<span
+                                        class="label label-rouded label-primary pull-right">2</span></span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="panel.php">داشبور</a></li>
+                        </ul>
+                    </li>
 
-            <!-- End PAge Content -->
+                    <li class="nav-label">مدیریت</li>
+                    <li><a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-envelope"></i><span
+                                    class="hide-menu">کاربران</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="user.php">افزودن کاربر</a></li>
+                            <li><a href="user-list.php">لیست کاربران</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li class="nav-label">فنی</li>
+                    <li><a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-columns"></i><span
+                                    class="hide-menu">تنظیمات</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="setting_site.php">تنظیمات سایت</a></li>
+                            <li><a href="setting.php">تنظیمات مدیریت</a></li>
+                            <li><a href="admin.php">تعریف مدیر</a></li>
+                            <li><a href="admin-list.php">لیست مدیران</a></li>
+                        </ul>
+                    </li>
+
+
+                </ul>
+            </nav>
+            <!-- End Sidebar navigation -->
         </div>
-
-<?
-include('footer.php');
-?>
+        <!-- End Sidebar scroll-->
+    </div>
+    <!-- End Left Sidebar  -->
