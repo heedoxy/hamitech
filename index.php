211c23a6906f300c2de21e1b640734a90d012f2a
--get <sxhadixr@gmail.com>
2021-04-06 21:26:08 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,110 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<? include('class/database.php'); ?>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>ورود به پنل مدیریت</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+</head>
+
+<body class="fix-header fix-sidebar">
+<!-- Preloader - style you can find in spinners.css -->
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+    </svg>
+</div>
+<!-- Main wrapper  -->
+<div id="main-wrapper">
+
+    <div class="unix-login">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-4">
+                    <div class="login-content card">
+                        <div class="login-form">
+                            <h4>ورود</h4>
+                            <?
+                            if (isset($_GET['error'])) {
+                                if ($_GET['error'] == 1) {
+                                    ?>
+                                    <div class="alert alert-danger">
+                                        نام کاربری یا پسورد درست وارد نشده است
+                                    </div>
+                                    <?
+                                }
+                            }
+                            ?>
+                            <form action="" method="POST">
+                                <div class="form-group">
+                                    <label>نام کاربری</label>
+                                    <input type="text" class="form-control" name="user" placeholder="نام کاربری">
+                                </div>
+                                <div class="form-group">
+                                    <label>پسورد</label>
+                                    <input type="password" class="form-control" name="pass" placeholder="پسورد">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> مرا بخاطر بسپار
+                                    </label>
+
+
+                                </div>
+                                <button type="submit" name="sub1" class="btn btn-primary btn-flat m-b-30 m-t-30">ورود
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- End Wrapper -->
+
+</div>
+<!-- End Wrapper -->
+<!-- All Jquery -->
+<script src="js/lib/jquery/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="js/lib/bootstrap/js/popper.min.js"></script>
+<script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Menu sidebar -->
+<script src="js/sidebarmenu.js"></script>
+<!--stickey kit -->
+<script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<!--Custom JavaScript -->
+<script src="js/scripts.js"></script>
+
+</body>
+<?
+if (isset($_POST['sub1'])) {
+    $action = new Action();
+    $user = $action->cleansql($_POST['user']);
+    $pass = $action->cleansql($_POST['pass']);
+    if ($action->adminlogin($user, $pass)) {
+        echo "<script type='text/javascript'>window.location.href = 'panel.php';</script>";
+    } else {
+        echo "<script type='text/javascript'>window.location.href = 'index.php?error=1';</script>";
+    }
+}
+?>
+</html>
