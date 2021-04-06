f9c30e46d1bd4517f81ef615117011c1b60632c7
--get <sxhadixr@gmail.com>
2021-04-06 21:21:26 +0430

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,94 @@
-<?php
+
+<script>
+    var customOptions = {
+        placeholder: "تاریخ شروع"
+        , twodigit: false
+        , closeAfterSelect: true
+        , nextButtonIcon: "fa fa-arrow-circle-right"
+        , previousButtonIcon: "fa fa-arrow-circle-left"
+        , buttonsColor: "black"
+        , forceFarsiDigits: true
+        , markToday: true
+        , markHolidays: true
+        , highlightSelectedDay: true
+        , sync: true
+        , gotoToday: true
+    }
+    kamaDatepicker('date_start', customOptions);
+
+    var customOptions = {
+        placeholder: "تاریخ پایان"
+        , twodigit: false
+        , closeAfterSelect: true
+        , nextButtonIcon: "fa fa-arrow-circle-right"
+        , previousButtonIcon: "fa fa-arrow-circle-left"
+        , buttonsColor: "black"
+        , forceFarsiDigits: true
+        , markToday: true
+        , markHolidays: true
+        , highlightSelectedDay: true
+        , sync: true
+        , gotoToday: true
+    }
+    kamaDatepicker('date_end', customOptions);
+</script>
+
+<!-- End Container fluid  -->
+<!-- footer -->
+<footer class="footer"> طراحی شده توسط <a href="http://hamitec.ir/">حامی تک</a></footer>
+<!-- End footer -->
+</div>
+<!-- End Page wrapper  -->
+</div>
+<!-- End Wrapper -->
+<!-- Bootstrap tether Core JavaScript -->
+<script src="js/lib/bootstrap/js/popper.min.js"></script>
+<script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Menu sidebar -->
+<script src="js/sidebarmenu.js"></script>
+<!--stickey kit -->
+<script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<!--Custom JavaScript -->
+
+
+<!-- Amchart -->
+<script src="js/lib/morris-chart/raphael-min.js"></script>
+<script src="js/lib/morris-chart/morris.js"></script>
+<script src="js/lib/morris-chart/dashboard1-init.js"></script>
+
+
+<script src="js/lib/calendar-2/moment.latest.min.js"></script>
+<!-- scripit init-->
+<script src="js/lib/calendar-2/semantic.ui.min.js"></script>
+<!-- scripit init-->
+<script src="js/lib/calendar-2/prism.min.js"></script>
+<!-- scripit init-->
+<script src="js/lib/calendar-2/pignose.calendar.min.js"></script>
+<!-- scripit init-->
+<script src="js/lib/calendar-2/pignose.init.js"></script>
+
+<script src="js/lib/owl-carousel/owl.carousel.min.js"></script>
+<script src="js/lib/owl-carousel/owl.carousel-init.js"></script>
+
+<!-- scripit init-->
+
+<script src="js/scripts.js"></script>
+
+<script src="js/lib/datatables/datatables.min.js"></script>
+<script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+<script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+<script src="js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+<script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+<script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+<script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+<script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+<script src="js/lib/datatables/datatables-init.js"></script>
+
+
+
+
+</body>
+
+</html>
