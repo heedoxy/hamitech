0346bf9139b0335e1acff282fd883bf1e11e5bc9
--get <sxhadixr@gmail.com>
2021-04-06 21:22:47 +0430

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,82 @@
-<?php
+<?
+include('header.php');
+?>
+    <!-- Page wrapper  -->
+    <div class="page-wrapper">
+
+        <!-- Bread crumb -->
+        <div class="row page-titles">
+            <div class="col-md-5 align-self-center text-right">
+                <h3 class="text-primary">داشبورد</h3> </div>
+            <div class="col-md-7 align-self-center text-left">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">خانه</a></li>
+                    <li class="breadcrumb-item active">داشبورد</li>
+                </ol>
+            </div>
+        </div>
+        <!-- End Bread crumb -->
+        <!-- Container fluid  -->
+        <div class="container-fluid">
+            <!-- Start Page Content -->
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="card p-30">
+                        <div class="media">
+                            <div class="media-left meida media-middle">
+                                <span><i class="fa fa-usd f-s-40 color-primary"></i></span>
+                            </div>
+                            <div class="media-body media-text-right">
+                                <h2>568120</h2>
+                                <p class="m-b-0">درآمد کل</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card p-30">
+                        <div class="media">
+                            <div class="media-left meida media-middle">
+                                <span><i class="fa fa-shopping-cart f-s-40 color-success"></i></span>
+                            </div>
+                            <div class="media-body media-text-right">
+                                <h2>1178</h2>
+                                <p class="m-b-0">فروش</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card p-30">
+                        <div class="media">
+                            <div class="media-left meida media-middle">
+                                <span><i class="fa fa-archive f-s-40 color-warning"></i></span>
+                            </div>
+                            <div class="media-body media-text-right">
+                                <h2>25</h2>
+                                <p class="m-b-0">سرویس کاران</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card p-30">
+                        <div class="media">
+                            <div class="media-left meida media-middle">
+                                <span><i class="fa fa-user f-s-40 color-danger"></i></span>
+                            </div>
+                            <div class="media-body media-text-right">
+                                <h2>847</h2>
+                                <p class="m-b-0">مشتریان</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- End PAge Content -->
+        </div>
+
+<?
+include('footer.php');
+?>
