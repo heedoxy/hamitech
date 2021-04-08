f3e76a111ab36370914d6b4f8bb3765db94b5d24
--get <sxhadixr@gmail.com>
2021-04-08 18:12:58 +0430

    Update panel.php

--- panel.php
+++ panel.php
@@ -6,12 +6,12 @@ include('header.php');
 
         <!-- Bread crumb -->
         <div class="row page-titles">
-            <div class="col-md-5 align-self-center text-right">
+            <div class="col-md-12 align-self-center text-right">
                 <h3 class="text-primary">داشبورد</h3> </div>
-            <div class="col-md-7 align-self-center text-left">
+            <div class="col-md-12 align-self-center text-right">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="javascript:void(0)">خانه</a></li>
-                    <li class="breadcrumb-item active">داشبورد</li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">داشبورد</a></li>
                 </ol>
             </div>
         </div>
