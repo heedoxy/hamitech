b508af61ba7401119accde4cd1724d1db7b8b58f
--get <sxhadixr@gmail.com>
2021-04-08 09:58:55 +0430

    Update footer.php

--- footer.php
+++ footer.php
@@ -85,6 +85,8 @@
 <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
 <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
 <script src="js/lib/datatables/datatables-init.js"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
 
 
 
