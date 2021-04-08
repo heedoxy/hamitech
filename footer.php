4d869e8a7e08892259c4bc5ab8efe44e6941fa44
--get <sxhadixr@gmail.com>
2021-04-08 21:06:39 +0430

    Update footer.php

--- footer.php
+++ footer.php
@@ -14,23 +14,11 @@
         , sync: true
         , gotoToday: true
     }
+    kamaDatepicker('date', customOptions);
     kamaDatepicker('date_start', customOptions);
-
-    var customOptions = {
-        placeholder: "تاریخ پایان"
-        , twodigit: false
-        , closeAfterSelect: true
-        , nextButtonIcon: "fa fa-arrow-circle-right"
-        , previousButtonIcon: "fa fa-arrow-circle-left"
-        , buttonsColor: "black"
-        , forceFarsiDigits: true
-        , markToday: true
-        , markHolidays: true
-        , highlightSelectedDay: true
-        , sync: true
-        , gotoToday: true
-    }
     kamaDatepicker('date_end', customOptions);
+    kamaDatepicker('birthday', customOptions);
+
 </script>
 
 <!-- End Container fluid  -->
