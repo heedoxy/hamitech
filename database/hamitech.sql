bcf35bcf73b2119bede20ceffca45d3a81b32f88
--get <sxhadixr@gmail.com>
2021-04-09 16:52:34 +0430

    Update database file

--- database/hamitech.sql
+++ database/hamitech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2021 at 08:58 AM
+-- Generation Time: Apr 09, 2021 at 02:21 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -28,22 +28,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_admin` (
-                             `id` int(11) NOT NULL,
-                             `fullname` varchar(100) NOT NULL,
-                             `phone` varchar(20) NOT NULL,
-                             `username` varchar(100) NOT NULL,
-                             `password` varchar(100) NOT NULL,
-                             `last_login` bigint(20) NOT NULL,
-                             `cdate` bigint(20) NOT NULL,
-                             `status` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `last_login` bigint(20) NOT NULL,
+  `created_at` bigint(20) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `access` tinyint(1) NOT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tbl_admin`
 --
 
-INSERT INTO `tbl_admin` (`id`, `fullname`, `phone`, `username`, `password`, `last_login`, `cdate`, `status`) VALUES
-(2, 'هادی :)', '09218248954', 'root', '1234', 1617951462, 0, 1);
+INSERT INTO `tbl_admin` (`id`, `first_name`, `last_name`, `phone`, `username`, `password`, `last_login`, `created_at`, `updated_at`, `access`, `status`) VALUES
+(1, 'سید هادی', 'رنجبر', '09218248954', 'root', '123456', 1617967982, 1617910804, 1617968287, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -52,17 +55,17 @@ INSERT INTO `tbl_admin` (`id`, `fullname`, `phone`, `username`, `password`, `las
 --
 
 CREATE TABLE `tbl_user` (
-                            `id` int(11) NOT NULL,
-                            `first_name` varchar(50) NOT NULL,
-                            `last_name` varchar(50) NOT NULL,
-                            `national_code` varchar(10) NOT NULL,
-                            `phone` varchar(20) NOT NULL,
-                            `username` varchar(50) NOT NULL,
-                            `password` varchar(50) NOT NULL,
-                            `birthday` bigint(20) NOT NULL,
-                            `created_at` bigint(20) NOT NULL,
-                            `updated_at` bigint(20) DEFAULT NULL,
-                            `status` tinyint(1) NOT NULL
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `national_code` varchar(10) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `birthday` bigint(20) NOT NULL,
+  `created_at` bigint(20) NOT NULL,
+  `updated_at` bigint(20) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -70,7 +73,7 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id`, `first_name`, `last_name`, `national_code`, `phone`, `username`, `password`, `birthday`, `created_at`, `updated_at`, `status`) VALUES
-(3, 'سید هادی', 'رنجبر', '0000000000', '09218248954', 'Hadi', 'root', 1616358600, 1617910804, 1617951471, 1);
+(1, 'سید هادی', 'رنجبر', '0000000000', '09218248954', 'hadi', '1234', 971901000, 1617967581, 1617970881, 1);
 
 --
 -- Indexes for dumped tables
@@ -80,13 +83,15 @@ INSERT INTO `tbl_user` (`id`, `first_name`, `last_name`, `national_code`, `phone
 -- Indexes for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -96,13 +101,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
