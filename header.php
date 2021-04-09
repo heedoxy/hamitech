be6d35d5fa51301b07f120eca846f29cd7c262a0
--get <sxhadixr@gmail.com>
2021-04-09 11:45:27 +0430

    Update header.php

--- header.php
+++ header.php
@@ -114,18 +114,7 @@ $user_id = $_SESSION['user_id'];
             <nav class="sidebar-nav">
                 <ul id="sidebarnav">
 
-                    <li class="nav-label">پنل</li>
-
-                    <li> <a class="has-arrow" href="panel.php" aria-expanded="false"><i class="fa fa-building"></i><span class="hide-menu">داشبورد</span></a></li>
-
-                    <hr class="m-0">
-
-                    <li class="nav-label">مدیریت</li>
-
-                    <li> <a class="has-arrow" href="user-list.php" aria-expanded="false"><i class="fa fa-building"></i><span class="hide-menu">کاربران</span></a></li>
-
-                    <hr class="m-0">
-
+                    <? include_once "sidebar.php"?>
 
                 </ul>
             </nav>
