5832319a7833d32a7393dc17bf444f9968c7688b
--get <sxhadixr@gmail.com>
2021-04-09 16:43:55 +0430

    Update panel theme

--- panel.php
+++ panel.php
@@ -23,59 +23,71 @@
             <div class="row">
 
                 <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-usd f-s-40 color-primary"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>568120</h2>
-                                <p class="m-b-0">درآمد کل</p>
+                    <a href="admin-list.php">
+                        <div class="card p-30 bg-warning">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-usd f-s-80 color-white"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <span class="text-white f-s-30">568120</span>
+                                    <br>
+                                    <span class="text-white f-s-20">مدیران</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-shopping-cart f-s-40 color-success"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>1178</h2>
-                                <p class="m-b-0">فروش</p>
+                    <a href="user-list.php">
+                        <div class="card p-30 bg-success">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-shopping-cart f-s-80 color-white"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <span class="text-white f-s-30">1178</span>
+                                    <br>
+                                    <span class="text-white f-s-20">کاربران</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-archive f-s-40 color-warning"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>25</h2>
-                                <p class="m-b-0">سرویس کاران</p>
+                    <a href="http://Hamitec.ir/" target="_blank">
+                        <div class="card p-30 bg-info">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-archive f-s-80 color-white"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <span class="text-white f-s-30">100</span>
+                                    <br>
+                                    <span class="text-white f-s-20">حامی تک</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
                 <div class="col-md-3">
-                    <div class="card p-30">
-                        <div class="media">
-                            <div class="media-left meida media-middle">
-                                <span><i class="fa fa-user f-s-40 color-danger"></i></span>
-                            </div>
-                            <div class="media-body media-text-right">
-                                <h2>847</h2>
-                                <p class="m-b-0">مشتریان</p>
+                    <a href="http://XeoHad.ir/" target="_blank">
+                        <div class="card p-30 bg-danger">
+                            <div class="media">
+                                <div class="media-left meida media-middle">
+                                    <span><i class="fa fa-user f-s-80 color-white"></i></span>
+                                </div>
+                                <div class="media-body media-text-right">
+                                    <span class="text-white f-s-30">100</span>
+                                    <br>
+                                    <span class="text-white f-s-20">من :)</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
             </div>
