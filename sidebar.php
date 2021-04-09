5222fb6921f990acaae314781e40b65fdcb826b2
--get <sxhadixr@gmail.com>
2021-04-09 11:44:32 +0430

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,11 @@
-<?php
+<li class="nav-label">پنل</li>
+
+<li> <a class="has-arrow" href="panel.php" aria-expanded="false"><i class="fa fa-building"></i><span class="hide-menu">داشبورد</span></a></li>
+
+<hr class="m-0">
+
+<li class="nav-label">مدیریت</li>
+
+<li> <a class="has-arrow" href="user-list.php" aria-expanded="false"><i class="fa fa-building"></i><span class="hide-menu">کاربران</span></a></li>
+
+<hr class="m-0">
