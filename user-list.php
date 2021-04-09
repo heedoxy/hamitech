e819cae9ded7fd46d0b888e5710f9b724c6bf9de
--get <sxhadixr@gmail.com>
2021-04-09 10:29:53 +0430

    back to version 2.0.0

--- user-list.php
+++ user-list.php
@@ -1,35 +1,26 @@
-<? require_once "class/database.php";
-
-$database = new DB();
-$connection = $database->connect();
+<?
+include('header.php');
+$edit = 0;
+$connect = new MyDB();
+$con = $connect->connect();
 $action = new Action();
 
-// ----------- check error ---------------------------------------------------------------------------------------------
-$error = 0;
-if (isset($_SESSION['error'])) {
-    $error = 1;
+$error=0;
+if(isset($_SESSION['error'])) {
+    $error=1;
     $error_val = $_SESSION['error'];
     unset($_SESSION['error']);
 }
-// ----------- check error ---------------------------------------------------------------------------------------------
-
-// ----------- get data ------------------------------------------------------------------------------------------------
-$counter = 1;
-$result = $connection->query("SELECT * FROM `tbl_user` ORDER BY `id` DESC");
-if (!$action->result($result)) return 0;
-// ----------- get data ------------------------------------------------------------------------------------------------
-
-// ----------- start html :) ------------------------------------------------------------------------------------------
-include('header.php'); ?>
+?>
 
     <div class="page-wrapper">
 
         <div class="row page-titles">
             <div class="col-md-12 align-self-center text-right">
-                <h3 class="text-primary">کاربران</h3></div>
+                <h3 class="text-primary">کاربران</h3> </div>
             <div class="col-md-12 align-self-center text-right">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="panel.php">خانه</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">خانه</a></li>
                     <li class="breadcrumb-item"><a href="javascript:void(0)">کاربران</a></li>
                 </ol>
             </div>
@@ -37,36 +28,35 @@ include('header.php'); ?>
 
         <div class="container-fluid">
 
-            <? if ($error) {
-                if ($error_val) { ?>
-                    <div class="alert alert-danger">
-                        عملیات ناموفق بود .
-                    </div>
-                <? } else { ?>
-                    <div class="alert alert-info text-right">
-                        عملیات موفق بود .
-                    </div>
-                <? }
-            } ?>
-
             <div class="row">
-                <a class="add-user mb-2" href="user.php"> ثبت کاربر <i class="fas fa-plus"></i></a>
+                <a class="add-user mb-2" href="user.php">ثبت کاربر <i class="fas fa-plus"></i></a>
             </div>
 
             <div class="row">
                 <div class="col-12">
 
+                    <? if($error) {
+                        if($error_val){  ?>
+                            <div class="alert alert-danger">
+                                عملیات ناموفق بود .
+                            </div>
+                        <? }else{ ?>
+                            <div class="alert alert-info text-right">
+                                عملیات موفق بود .
+                            </div>
+                        <? } } ?>
+
                     <div class="card">
                         <div class="card-body">
 
                             <div class="table-responsive m-t-5">
-                                <table id="example23" class="display nowrap table table-hover table-striped"
+                                <table id="example23"
+                                       class="display nowrap table table-hover table-striped table-bordered"
                                        cellspacing="0" width="100%">
                                     <thead>
                                     <tr>
                                         <th class="text-center">ردیف</th>
                                         <th class="text-center">نام</th>
-                                        <th class="text-center">نام خانوادگی</th>
                                         <th class="text-center">کدملی</th>
                                         <th class="text-center">وضعیت</th>
                                         <th class="text-center">مدیریت</th>
@@ -74,33 +64,36 @@ include('header.php'); ?>
                                     </thead>
 
                                     <tbody class="text-center">
-                                    <? while ($row = $result->fetch_object()) { ?>
+                                    <?
+                                    $counter = 1;
+                                    $result = mysqli_query($con, "SELECT * FROM tbl_user");
+                                    if (!$result) {
+                                        echo mysqli_errno($this->_conn) . mysqli_error($this->_conn);
+                                        return false;
+                                    }
+                                    while($row = mysqli_fetch_assoc($result))
+                                    {
+
+                                        ?>
                                         <tr class="text-center">
 
-                                            <td class="text-center"><?= $counter++ ?></td>
-                                            <td class="text-center"><?= $row->first_name ?></td>
-                                            <td class="text-center"><?= $row->last_name ?></td>
-                                            <td class="text-center"><?= $row->national_code ?></td>
-
-                                            <td class="text-center">
-                                                <?
-                                                if ($row->status == 1) echo "<status-indicator positive pulse></status-indicator>";
+                                            <td class="text-center"><? echo $counter++; ?></td>
+                                            <td class="text-center"><? echo $row['fullname']; ?></td>
+                                            <td class="text-center"><? echo $row['codemeli']; ?></td>
+                                            <td class="text-center"><?
+                                                if($row['status']==1) echo "<status-indicator positive pulse></status-indicator>";
                                                 else echo "<status-indicator negative pulse></status-indicator>";
-                                                ?>
-                                            </td>
-
+                                                ?></td>
                                             <td class="text-center">
-                                                <a href="user.php?edit=<?= $row->id ?>">
-                                                    <i class="fa fa-pencil-square-o"></i>
-                                                </a>
+                                                <a href="user.php?edit=<? echo $row['id']; ?>"><i class="fa fa-pencil-square-o"></i></a>
                                                 |
-                                                <a href="user.php?remove=<?= $row->id ?>">
-                                                    <i class="fa fa-trash"></i>
-                                                </a>
+                                                <a href="user.php?remove=<? echo $row['id']; ?>"><i class="fa fa-trash"></i></a>
                                             </td>
 
                                         </tr>
-                                    <? } ?>
+                                        <?
+                                    }
+                                    ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -108,7 +101,11 @@ include('header.php'); ?>
                     </div>
                 </div>
             </div>
+
         </div>
+
     </div>
 
-<? include('footer.php'); ?>
+<?
+include('footer.php');
+?>
