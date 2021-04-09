fcd7a6265f59720d47f64904db47bed208deeb90
--get <sxhadixr@gmail.com>
2021-04-09 14:35:32 +0430

    Update user.php

--- user.php
+++ user.php
@@ -2,6 +2,7 @@
 $database = new DB();
 $connection = $database->connect();
 $action = new Action();
+$url = $action->url();
 
 // ----------- get data from database when action is edit --------------------------------------------------------------
 $edit = false;
@@ -39,7 +40,6 @@ if (isset($_POST['submit'])) {
 
     // send query
     if ($edit) {
-        $id = $action->request('edit');
         $command = $action->user_edit($id, $first_name, $last_name, $national_code, $phone, $username, $password, $birthday, $status);
     } else {
         $command = $action->user_add($first_name, $last_name, $national_code, $phone, $username, $password, $birthday, $status);
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
     }
 
     // bye bye :)
-    header("Location: user.php?edit=$command");
+    header("Location: $url");
 
 }
 // ----------- add or edit ---------------------------------------------------------------------------------------------
@@ -69,150 +69,151 @@ if (isset($_GET['remove'])) {
 // ----------- start html :) ------------------------------------------------------------------------------------------
 include('header.php'); ?>
 
-    <div class="page-wrapper">
+<div class="page-wrapper">
 
-        <div class="row page-titles">
+    <div class="row page-titles">
 
-            <!-- ----------- start title --------------------------------------------------------------------------- -->
-            <div class="col-md-12 align-self-center text-right">
-                <?php if (!isset($_GET['action'])) { ?>
-                    <h3 class="text-primary">ثبت کاربر</h3>
+        <!-- ----------- start title --------------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <?php if (!isset($_GET['action'])) { ?>
+                <h3 class="text-primary">ثبت کاربر</h3>
+            <?php } else { ?>
+                <h3 class="text-primary">ویرایش کاربر</h3>
+            <?php } ?>
+        </div>
+        <!-- ----------- end title ----------------------------------------------------------------------------- -->
+
+        <!-- ----------- start breadcrumb ---------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="panel.php">خانه</a></li>
+                <li class="breadcrumb-item"><a href="user-list.php">کاربران</a></li>
+                <?php if ($edit) { ?>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">ثبت</a></li>
                 <?php } else { ?>
-                    <h3 class="text-primary">ویرایش کاربر</h3>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">ویرایش</a></li>
                 <?php } ?>
-            </div>
-            <!-- ----------- end title ----------------------------------------------------------------------------- -->
-
-            <!-- ----------- start breadcrumb ---------------------------------------------------------------------- -->
-            <div class="col-md-12 align-self-center text-right">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="panel.php">خانه</a></li>
-                    <li class="breadcrumb-item"><a href="user-list.php">کاربران</a></li>
-                    <?php if ($edit) { ?>
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">ثبت</a></li>
-                    <?php } else { ?>
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">ویرایش</a></li>
-                    <?php } ?>
-                </ol>
-            </div>
-            <!-- ----------- end breadcrumb ------------------------------------------------------------------------ -->
-
+            </ol>
         </div>
-
-        <!-- ----------- start main container ---------------------------------------------------------------------- -->
-        <div class="container-fluid">
-
-            <!-- ----------- start error list ---------------------------------------------------------------------- -->
-            <? if ($error) {
-                if ($error_val) { ?>
-                    <div class="alert alert-danger">
-                        عملیات ناموفق بود .
-                    </div>
-                <? } else { ?>
-                    <div class="alert alert-info text-right">
-                        عملیات موفق بود .
-                    </div>
-                <? }
-            } ?>
-            <!-- ----------- end error list ------------------------------------------------------------------------ -->
-
-            <div class="row">
-                <div class="col-lg-6">
-
-                    <!-- ----------- start history ----------------------------------------------------------------- -->
-                    <? if ($edit) { ?>
-                        <div class="row m-b-0">
-                            <div class="col-lg-6">
-                                <p class="text-right m-b-0">
-                                    تاریخ ثبت :
-                                    <?= $action->time_to_shamsi($row->created_at) ?>
-                                </p>
-                            </div>
-                            <? if ($row->updated_at) { ?>
-                                <div class="col-lg-6">
-                                    <p class="text-right m-b-0">
-                                        آخرین ویرایش :
-                                        <?= $action->time_to_shamsi($row->updated_at) ?>
-                                    </p>
-                                </div>
-                            <? } ?>
-                        </div>
-                    <? } ?>
-                    <!-- ----------- end history ------------------------------------------------------------------- -->
-
-                    <!-- ----------- start row of fields ----------------------------------------------------------- -->
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="basic-form">
-                                <form action="" method="post" enctype="multipart/form-data">
-
-                                    <div class="form-group">
-                                        <input type="text" name="first_name" class="form-control input-default "
-                                               placeholder="نام"
-                                               value="<?= ($edit) ? $row->first_name : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" name="last_name" class="form-control input-default "
-                                               placeholder="نام خانوادگی"
-                                               value="<?= ($edit) ? $row->last_name : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="number" name="national_code" class="form-control"
-                                               placeholder="کدملی"
-                                               value="<?= ($edit) ? $row->national_code : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" name="phone" class="form-control input-default "
-                                               placeholder="تلفن همراه"
-                                               value="<?= ($edit) ? $row->phone : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" name="username" class="form-control input-default "
-                                               placeholder="نام کاربری"
-                                               value="<?= ($edit) ? $row->username : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" name="password" class="form-control input-default "
-                                               placeholder="رمز عبور"
-                                               value="<?= ($edit) ? $row->password : "" ?>">
-                                    </div>
-
-                                    <div class="form-group">
-                                        <input type="text" id="date" name="birthday" class="form-control"
-                                               placeholder="تاریخ تولد"
-                                               value="<?= ($edit) ? $action->time_to_shamsi($row->birthday) : "" ?>">
-                                    </div>
-
-                                    <div class="form-actions">
-
-                                        <label class="float-right">
-                                            <input type="checkbox" class="float-right m-1" name="status" value="1"
-                                                <? if ($edit && $row->status) echo "checked"; ?> >
-                                            فعال
-                                        </label>
-
-                                        <button type="submit" name="submit" class="btn btn-success sweet-success">
-                                            <i class="fa fa-check"></i> ثبت
-                                        </button>
-                                        <a href="user-list.php"><span name="back" class="btn btn-inverse">بازگشت</span></a>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- ----------- end row of fields ----------------------------------------------------------- -->
-
-                </div>
-            </div>
-        </div>
-        <!-- ----------- end main container ------------------------------------------------------------------------ -->
+        <!-- ----------- end breadcrumb ------------------------------------------------------------------------ -->
 
     </div>
+
+    <!-- ----------- start main container ---------------------------------------------------------------------- -->
+    <div class="container-fluid">
+
+        <!-- ----------- start error list ---------------------------------------------------------------------- -->
+        <? if ($error) {
+            if ($error_val) { ?>
+                <div class="alert alert-danger">
+                    عملیات ناموفق بود .
+                </div>
+            <? } else { ?>
+                <div class="alert alert-info text-right">
+                    عملیات موفق بود .
+                </div>
+            <? }
+        } ?>
+        <!-- ----------- end error list ------------------------------------------------------------------------ -->
+
+        <div class="row">
+            <div class="col-lg-6">
+
+                <!-- ----------- start history ----------------------------------------------------------------- -->
+                <? if ($edit) { ?>
+                    <div class="row m-b-0">
+                        <div class="col-lg-6">
+                            <p class="text-right m-b-0">
+                                تاریخ ثبت :
+                                <?= $action->time_to_shamsi($row->created_at) ?>
+                            </p>
+                        </div>
+                        <? if ($row->updated_at) { ?>
+                            <div class="col-lg-6">
+                                <p class="text-right m-b-0">
+                                    آخرین ویرایش :
+                                    <?= $action->time_to_shamsi($row->updated_at) ?>
+                                </p>
+                            </div>
+                        <? } ?>
+                    </div>
+                <? } ?>
+                <!-- ----------- end history ------------------------------------------------------------------- -->
+
+                <!-- ----------- start row of fields ----------------------------------------------------------- -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <form action="" method="post" enctype="multipart/form-data">
+
+                                <div class="form-group">
+                                    <input type="text" name="first_name" class="form-control input-default "
+                                           placeholder="نام"
+                                           value="<?= ($edit) ? $row->first_name : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="last_name" class="form-control input-default "
+                                           placeholder="نام خانوادگی"
+                                           value="<?= ($edit) ? $row->last_name : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="number" name="national_code" class="form-control"
+                                           placeholder="کدملی"
+                                           value="<?= ($edit) ? $row->national_code : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="phone" class="form-control input-default "
+                                           placeholder="تلفن همراه"
+                                           value="<?= ($edit) ? $row->phone : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="username" class="form-control input-default "
+                                           placeholder="نام کاربری"
+                                           value="<?= ($edit) ? $row->username : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="password" class="form-control input-default "
+                                           placeholder="رمز عبور"
+                                           value="<?= ($edit) ? $row->password : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" id="date" name="birthday" class="form-control"
+                                           placeholder="تاریخ تولد"
+                                           value="<?= ($edit) ? $action->time_to_shamsi($row->birthday) : "" ?>"
+                                           required>
+                                </div>
+
+                                <div class="form-actions">
+
+                                    <label class="float-right">
+                                        <input type="checkbox" class="float-right m-1" name="status" value="1"
+                                            <? if ($edit && $row->status) echo "checked"; ?> >
+                                        فعال
+                                    </label>
+
+                                    <button type="submit" name="submit" class="btn btn-success sweet-success">
+                                        <i class="fa fa-check"></i> ثبت
+                                    </button>
+                                    <a href="user-list.php"><span name="back" class="btn btn-inverse">بازگشت</span></a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- ----------- end row of fields ----------------------------------------------------------- -->
+
+            </div>
+        </div>
+    </div>
+    <!-- ----------- end main container ------------------------------------------------------------------------ -->
+
+</div>
 <? include('footer.php'); ?>
 // ----------- end html :) ---------------------------------------------------------------------------------------------
 
