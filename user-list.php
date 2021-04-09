762fa7d097d90d85ad3b7871cd71f6bf4a906f9f
--get <sxhadixr@gmail.com>
2021-04-09 10:16:26 +0430

    Back to last version user-list

--- user-list.php
+++ user-list.php
@@ -1,106 +1,75 @@
-<? require_once "class/database.php";
-
-$database = new DB();
-$connection = $database->connect();
+<?
+include('header.php');
+$edit = 0;
+$connect = new MyDB();
+$con = $connect->connect();
 $action = new Action();
-
-// ----------- check error ---------------------------------------------------------------------------------------------
-$error = 0;
-if (isset($_SESSION['error'])) {
-    $error = 1;
-    $error_val = $_SESSION['error'];
-    unset($_SESSION['error']);
-}
-// ----------- check error ---------------------------------------------------------------------------------------------
-
-// ----------- get data ------------------------------------------------------------------------------------------------
-$counter = 1;
-$result = $connection->query("SELECT * FROM `tbl_user` ORDER BY `id` DESC");
-if (!$action->result($result)) return 0;
-// ----------- get data ------------------------------------------------------------------------------------------------
-
-// ----------- start html :) ------------------------------------------------------------------------------------------
-include('header.php'); ?>
+?>
 
     <div class="page-wrapper">
 
         <div class="row page-titles">
-            <div class="col-md-12 align-self-center text-right">
-                <h3 class="text-primary">کاربران</h3></div>
-            <div class="col-md-12 align-self-center text-right">
+            <div class="col-md-5 align-self-center text-right">
+                <h3 class="text-primary">لیست کاربران</h3></div>
+            <div class="col-md-7 align-self-center text-left">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="panel.php">خانه</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">خانه</a></li>
                     <li class="breadcrumb-item"><a href="javascript:void(0)">کاربران</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">لیست کاربران</a></li>
                 </ol>
             </div>
         </div>
 
         <div class="container-fluid">
 
-            <? if ($error) {
-                if ($error_val) { ?>
-                    <div class="alert alert-danger">
-                        عملیات ناموفق بود .
-                    </div>
-                <? } else { ?>
-                    <div class="alert alert-info text-right">
-                        عملیات موفق بود .
-                    </div>
-                <? }
-            } ?>
-
-            <div class="row">
-                <a class="add-user mb-2" href="user.php"> ثبت کاربر <i class="fas fa-plus"></i></a>
-            </div>
-
             <div class="row">
                 <div class="col-12">
-
                     <div class="card">
                         <div class="card-body">
 
                             <div class="table-responsive m-t-5">
-                                <table id="example23" class="display nowrap table table-hover table-striped"
+                                <table id="example23"
+                                       class="display nowrap table table-hover table-striped table-bordered"
                                        cellspacing="0" width="100%">
                                     <thead>
                                     <tr>
-                                        <th class="text-center">ردیف</th>
-                                        <th class="text-center">نام</th>
-                                        <th class="text-center">نام خانوادگی</th>
-                                        <th class="text-center">کدملی</th>
-                                        <th class="text-center">وضعیت</th>
-                                        <th class="text-center">مدیریت</th>
+                                        <th>ردیف</th>
+                                        <th>نام</th>
+                                        <th>کد ملی</th>
+                                        <th>شماره تماس</th>
+                                        <th>ویرایش</th>
+                                        <th>حذف</th>
                                     </tr>
                                     </thead>
 
                                     <tbody class="text-center">
-                                    <? while ($row = $result->fetch_object()) { ?>
+                                    <?
+                                    $counter = 1;
+                                    $result = mysqli_query($con, "SELECT * FROM tbl_user");
+                                    if (!$result) {
+                                        echo mysqli_errno($this->_conn) . mysqli_error($this->_conn);
+                                        return false;
+                                    }
+                                    while ($row = $result->fetch_row()) {
+                                        ?>
                                         <tr class="text-center">
 
-                                            <td class="text-center"><?= $counter++ ?></td>
-                                            <td class="text-center"><?= $row->first_name ?></td>
-                                            <td class="text-center"><?= $row->last_name ?></td>
-                                            <td class="text-center"><?= $row->national_code ?></td>
-
+                                            <td><? echo $counter++; ?></td>
+                                            <td><? echo $row[1] ?></td>
+                                            <td><? echo $row[3] ?></td>
+                                            <td><? echo $row[2] ?></td>
+                                            <td><a href="user.php?action=edit&id=<? echo $row[0]; ?>"><i
+                                                            class="fa fa-pencil-square-o"></i></a></td>
                                             <td class="text-center">
-                                                <?
-                                                if ($row->status == 1) echo "<status-indicator positive pulse></status-indicator>";
-                                                else echo "<status-indicator negative pulse></status-indicator>";
-                                                ?>
-                                            </td>
-
-                                            <td class="text-center">
-                                                <a href="user.php?edit=<?= $row->id ?>">
-                                                    <i class="fa fa-pencil-square-o"></i>
-                                                </a>
-                                                |
-                                                <a href="user.php?remove=<?= $row->id ?>">
-                                                    <i class="fa fa-trash"></i>
+                                                <a href="user.php?action=remove&id=<? echo $row[0]; ?>">
+                                                    <i class="fa fa-trash-o"></i>
                                                 </a>
                                             </td>
 
                                         </tr>
-                                    <? } ?>
+                                        <?
+                                    }
+                                    ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -108,7 +77,11 @@ include('header.php'); ?>
                     </div>
                 </div>
             </div>
+
         </div>
+
     </div>
 
-<? include('footer.php'); ?>
+<?
+include('footer.php');
+?>
