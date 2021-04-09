0b99de617f1135a4217dbd59efbc727ed9a8fea4
--get <sxhadixr@gmail.com>
2021-04-09 15:08:57 +0430

    Update admin.php

---
+++ admin.php
@@ -0,0 +1,216 @@
+<? require_once "class/database.php";
+$database = new DB();
+$connection = $database->connect();
+$action = new Action();
+$url = $action->url();
+$list = "admin-list";
+
+// ----------- get data from database when action is edit --------------------------------------------------------------
+$edit = false;
+if (isset($_GET['edit'])) {
+    $edit = true;
+    $id = $action->request('edit');
+    $result = $connection->query("SELECT * FROM tbl_admin WHERE `id`='$id' AND NOT `access`='0' ");
+    if (!$action->result($result)) return false;
+    if (!$result->num_rows) header("Location: admin-list.php");
+    $row = $result->fetch_object();
+}
+// ----------- get data from database when action is edit --------------------------------------------------------------
+
+// ----------- check error ---------------------------------------------------------------------------------------------
+$error = false;
+if (isset($_SESSION['error'])) {
+    $error = true;
+    $error_val = $_SESSION['error'];
+    unset($_SESSION['error']);
+}
+// ----------- check error ---------------------------------------------------------------------------------------------
+
+// ----------- add or edit ---------------------------------------------------------------------------------------------
+if (isset($_POST['submit'])) {
+
+    // get fields
+    $first_name = $action->request('first_name');
+    $last_name = $action->request('last_name');
+    $phone = $action->request('phone');
+    $username = $action->request('username');
+    $password = $action->request('password');
+    $access = 0;
+    $status = $action->request('status');
+
+    // send query
+    if ($edit) {
+        $command = $action->admin_edit($id, $first_name, $last_name, $phone, $username, $password, $status, $access);
+    } else {
+        $command = $action->admin_add($first_name, $last_name, $phone, $username, $password, $status, $access);
+    }
+
+    // check errors
+    if ($command) {
+        $_SESSION['error'] = 0;
+    } else {
+        $_SESSION['error'] = 1;
+    }
+
+    // bye bye :)
+    header("Location: $url");
+
+}
+// ----------- add or edit ---------------------------------------------------------------------------------------------
+
+// ----------- delete --------------------------------------------------------------------------------------------------
+if (isset($_GET['remove'])) {
+    $id = $action->request('remove');
+    $_SESSION['error'] = !$action->admin_remove($id);
+    header("Location: $list");
+}
+// ----------- delete --------------------------------------------------------------------------------------------------
+
+// ----------- change status -------------------------------------------------------------------------------------------
+if (isset($_GET['status'])) {
+    $id = $action->request('status');
+    $_SESSION['error'] = !$action->admin_status($id);
+    header("Location: $list");
+}
+// ----------- change status -------------------------------------------------------------------------------------------
+
+// ----------- start html :) ------------------------------------------------------------------------------------------
+include('header.php'); ?>
+
+<div class="page-wrapper">
+
+    <div class="row page-titles">
+
+        <!-- ----------- start title --------------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <?php if (!isset($_GET['action'])) { ?>
+                <h3 class="text-primary">ثبت مدیر</h3>
+            <?php } else { ?>
+                <h3 class="text-primary">ویرایش مدیر</h3>
+            <?php } ?>
+        </div>
+        <!-- ----------- end title ----------------------------------------------------------------------------- -->
+
+        <!-- ----------- start breadcrumb ---------------------------------------------------------------------- -->
+        <div class="col-md-12 align-self-center text-right">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="panel.php">خانه</a></li>
+                <li class="breadcrumb-item"><a href="user-list.php">مدیران</a></li>
+                <?php if ($edit) { ?>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">ثبت</a></li>
+                <?php } else { ?>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">ویرایش</a></li>
+                <?php } ?>
+            </ol>
+        </div>
+        <!-- ----------- end breadcrumb ------------------------------------------------------------------------ -->
+
+    </div>
+
+    <!-- ----------- start main container ---------------------------------------------------------------------- -->
+    <div class="container-fluid">
+
+        <!-- ----------- start error list ---------------------------------------------------------------------- -->
+        <? if ($error) {
+            if ($error_val) { ?>
+                <div class="alert alert-danger">
+                    عملیات ناموفق بود .
+                </div>
+            <? } else { ?>
+                <div class="alert alert-info text-right">
+                    عملیات موفق بود .
+                </div>
+            <? }
+        } ?>
+        <!-- ----------- end error list ------------------------------------------------------------------------ -->
+
+        <div class="row">
+            <div class="col-lg-6">
+
+                <!-- ----------- start history ----------------------------------------------------------------- -->
+                <? if ($edit) { ?>
+                    <div class="row m-b-0">
+                        <div class="col-lg-6">
+                            <p class="text-right m-b-0">
+                                تاریخ ثبت :
+                                <?= $action->time_to_shamsi($row->created_at) ?>
+                            </p>
+                        </div>
+                        <? if ($row->updated_at) { ?>
+                            <div class="col-lg-6">
+                                <p class="text-right m-b-0">
+                                    آخرین ویرایش :
+                                    <?= $action->time_to_shamsi($row->updated_at) ?>
+                                </p>
+                            </div>
+                        <? } ?>
+                    </div>
+                <? } ?>
+                <!-- ----------- end history ------------------------------------------------------------------- -->
+
+                <!-- ----------- start row of fields ----------------------------------------------------------- -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <form action="" method="post" enctype="multipart/form-data">
+
+                                <div class="form-group">
+                                    <input type="text" name="first_name" class="form-control input-default "
+                                           placeholder="نام"
+                                           value="<?= ($edit) ? $row->first_name : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="last_name" class="form-control input-default "
+                                           placeholder="نام خانوادگی"
+                                           value="<?= ($edit) ? $row->last_name : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="phone" class="form-control input-default "
+                                           placeholder="تلفن همراه"
+                                           value="<?= ($edit) ? $row->phone : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="username" class="form-control input-default "
+                                           placeholder="نام کاربری"
+                                           value="<?= ($edit) ? $row->username : "" ?>" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" name="password" class="form-control input-default "
+                                           placeholder="رمز عبور"
+                                           value="<?= ($edit) ? $row->password : "" ?>" required>
+                                </div>
+
+                                <div class="form-actions">
+
+                                    <label class="float-right">
+                                        <input type="checkbox" class="float-right m-1" name="status" value="1"
+                                            <? if ($edit && $row->status) echo "checked"; ?> >
+                                        فعال
+                                    </label>
+
+                                    <button type="submit" name="submit" class="btn btn-success sweet-success">
+                                        <i class="fa fa-check"></i> ثبت
+                                    </button>
+
+                                    <a href="<?= $list ?>"><span name="back" class="btn btn-inverse">بازگشت</span></a>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- ----------- end row of fields ----------------------------------------------------------- -->
+
+            </div>
+        </div>
+    </div>
+    <!-- ----------- end main container ------------------------------------------------------------------------ -->
+
+</div>
+<? include('footer.php'); ?>
+// ----------- end html :) ---------------------------------------------------------------------------------------------
+
